6456761cc7dda27cdbeee9865b6fc9ac33ac2b00
buhaymarella <marella.gutierrez84@gmail.com>
2023-01-12 21:34:14 +0800

    Added Javascript function

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,37 @@
 //gpa formula =  -4 / number of items * score + 5
 // gpa => 1 - 3 text color green
 // gpa => 3 - 5 text color red
+
+function calcGpa(){
+    const score = parseInt(document.getElementById("score").value);
+    const noItems = parseInt(document.getElementById("noItems").value);
+
+    scoreGpa.value = -4 / noItems * score + 5;
+
+    if (scoreGpa.value <= 3){
+        document.getElementById("rem").innerHTML = "Passed";
+        document.getElementById("rem").style.color = "green";
+        document.getElementById("rem").style.textShadow = "none";
+    }
+    else{
+        document.getElementById("rem").innerHTML = "Failed";
+        document.getElementById("rem").style.color = "red";
+        document.getElementById("rem").style.textShadow = "none";
+    }
+
+}
+
+document.getElementById("scoreGpa").oninput = scoreGpa.value;
+
+
+function reset(){
+    document.getElementById("noItems").oninput = " ";
+    document.getElementById("score").oninput = " ";
+    document.getElementById("scoreGpa").oninput = " ";
+}
+
+
+
+
+
+
